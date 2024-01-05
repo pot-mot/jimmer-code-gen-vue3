82c1eea532de0a48fc05506035bada0dc35fa049
Potmot <377029227@qq.com>
2024-01-05 18:50:53 +0800

    2024/1/5 补充阻止 wrapper 被序列化

--- src/components/business/modelGraphEditor/tableNode/tableNodeData.ts
+++ src/components/business/modelGraphEditor/tableNode/tableNodeData.ts
@@ -3,4 +3,5 @@ import {Node} from '@antv/x6'
 
 export const updateTableNodeData = (node: Node, newTable: GenTableModelInput) => {
     node.setData({wrapper: node.getData().wrapper, table: newTable}, {overwrite: true})
+    Object.defineProperty(node.getData(), 'wrapper', { enumerable: false, writable: true })
 }
