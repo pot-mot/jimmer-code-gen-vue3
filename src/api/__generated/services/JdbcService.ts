83356afe4282f7c6c4088428044c5cc54498ef01
Potmot <377029227@qq.com>
2024-01-17 17:14:48 +0800

    2024/1/17 修正 git 配置，变更 JdbcService 的名称

---
+++ src/api/__generated/services/JdbcService.ts
@@ -0,0 +1,16 @@
+import type {Executor} from '../';
+
+export class JdbcService {
+
+    constructor(private executor: Executor) {}
+
+    async listType(): Promise<
+        {[key:string]: number}
+    > {
+        let _uri = '/jdbc/type';
+        return (await this.executor({uri: _uri, method: 'GET'})) as Promise<{[key:string]: number}>;
+    }
+}
+export type JdbcServiceOptions = {
+    'listType': {}
+}
