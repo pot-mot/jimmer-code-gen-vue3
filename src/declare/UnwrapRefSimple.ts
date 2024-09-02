8dcda170d06cf271ad7e2963cf6c23df9213050c
Potmot <377029227@qq.com>
2024-09-03 01:54:47 +0800

    2024-9-3 升级项目依赖版本

--- src/declare/UnwrapRefSimple.ts
+++ src/declare/UnwrapRefSimple.ts
@@ -1,6 +1,13 @@
-import {BaseTypes, RawSymbol, Ref, RefUnwrapBailTypes, ShallowReactiveMarker, UnwrapRef} from "@vue/reactivity";
+import {
+    Builtin,
+    RawSymbol,
+    Ref,
+    RefUnwrapBailTypes,
+    ShallowReactiveMarker,
+    UnwrapRef,
+} from "@vue/reactivity";
 
-export type UnwrapRefSimple<T> = T extends Function | BaseTypes | Ref | RefUnwrapBailTypes[keyof RefUnwrapBailTypes] | {
+export type UnwrapRefSimple<T> = T extends Builtin | Ref | RefUnwrapBailTypes[keyof RefUnwrapBailTypes] | {
     [RawSymbol]?: true;
 } ? T : T extends Map<infer K, infer V> ? Map<K, UnwrapRefSimple<V>> & UnwrapRef<Omit<T, keyof Map<any, any>>> : T extends WeakMap<infer K, infer V> ? WeakMap<K, UnwrapRefSimple<V>> & UnwrapRef<Omit<T, keyof WeakMap<any, any>>> : T extends Set<infer V> ? Set<UnwrapRefSimple<V>> & UnwrapRef<Omit<T, keyof Set<any>>> : T extends WeakSet<infer V> ? WeakSet<UnwrapRefSimple<V>> & UnwrapRef<Omit<T, keyof WeakSet<any>>> : T extends ReadonlyArray<any> ? {
     [K in keyof T]: UnwrapRefSimple<T[K]>;
