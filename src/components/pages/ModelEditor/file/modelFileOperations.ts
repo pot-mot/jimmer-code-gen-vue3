3a6720df56b8f3649e060ee06bd7be9bb1e3abb1
Potmot <377029227@qq.com>
2024-10-19 11:13:58 +0800

    FIX: 修正 modelFileOperations 压缩 zip 时的类型

--- src/components/pages/ModelEditor/file/modelFileOperations.ts
+++ src/components/pages/ModelEditor/file/modelFileOperations.ts
@@ -1,4 +1,4 @@
-import {GenModelView, Pair} from "@/api/__generated/model/static";
+import {GenerateFile, GenModelView} from "@/api/__generated/model/static";
 import JSZip from "jszip";
 import {api} from "@/api";
 import {saveAs} from "file-saver";
@@ -8,19 +8,19 @@ import {sendI18nMessage} from "@/message/message.ts";
 import {getModelAllCopyData} from "@/components/pages/ModelEditor/graph/clipBoard/clipBoard.ts";
 import {GenerateType} from "@/api/__generated/model/enums";
 
-const createZip = async (files: Array<Pair<string, string>>): Promise<Blob> => {
+const createZip = async (files: Array<Pick<GenerateFile, 'path' | 'content'>>): Promise<Blob> => {
     const zip = new JSZip()
 
     const nameCountMap = new Map<string, number>
 
     files.forEach(it => {
-        if (nameCountMap.has(it.first)) {
-            const count = nameCountMap.get(it.first)!
-            nameCountMap.set(it.first, count + 1)
-            zip.file(`${it.first}(${count})`, it.second)
+        if (nameCountMap.has(it.path)) {
+            const count = nameCountMap.get(it.path)!
+            nameCountMap.set(it.path, count + 1)
+            zip.file(`${it.path}(${count})`, it.content)
         } else {
-            nameCountMap.set(it.first, 1)
-            zip.file(it.first, it.second)
+            nameCountMap.set(it.path, 1)
+            zip.file(it.path, it.content)
         }
     })
 
@@ -68,9 +68,9 @@ export const downloadModelZip = async (model: GenModelView) => {
 
     const {id, ...other} = model
     const modelFiles = [
-        {first: "model/model.json", second: jsonPrettyFormat(other)},
-        {first: "model/graph-data.json", second: jsonStrPrettyFormat(model.graphData)},
-        {first: "model/pure-data.json", second: jsonPrettyFormat(copyData)}
+        {path: "model/model.json", content: jsonPrettyFormat(other)},
+        {path: "model/graph-data.json", content: jsonStrPrettyFormat(model.graphData)},
+        {path: "model/pure-data.json", content: jsonPrettyFormat(copyData)}
     ]
 
     const file = await createZip([...modelFiles, ...codes])
