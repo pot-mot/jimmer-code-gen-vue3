37438085672f8e7819c1762705b52215fcc81cf4
Potmot <377029227@qq.com>
2024-10-24 11:57:23 +0800

    UPDATE: 更新 api GenerateTag 和 GenerateType

--- src/api/__generated/model/enums/GenerateType.ts
+++ src/api/__generated/model/enums/GenerateType.ts
@@ -8,6 +8,8 @@ export const GenerateType_CONSTANTS = [
     'DTO', 
     'FrontEnd', 
     'EnumComponent', 
-    'View'
+    'View', 
+    'Permission', 
+    'Route'
 ] as const;
 export type GenerateType = typeof GenerateType_CONSTANTS[number];
