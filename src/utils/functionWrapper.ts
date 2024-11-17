216fea25d1c3c3a179837e2489366a724ce32a31
Potmot <377029227@qq.com>
2024-11-17 18:41:19 +0800

    UPDATE: 调整 functionWrapper 使其在异常时终止

--- src/utils/functionWrapper.ts
+++ src/utils/functionWrapper.ts
@@ -5,12 +5,19 @@ export type WrapperFunction<F extends (...args: any) => any> = (...args: Paramet
 export const functionWrapper = <F extends (...args: any) => any>(
     fn: F,
     before: (args: Parameters<F>) => any,
-    after: (args: Parameters<F>, result: ReturnType<F>) => any,
+    after: (args: Parameters<F>, result: ReturnType<F> | undefined, error?: any, ) => any,
 ): WrapperFunction<F> => {
     return async (...args: Parameters<F>): Promise<UnwrapPromise<ReturnType<F>>> => {
-        await before(args)
-        const result = await fn(...args)
-        await after(args, result)
-        return result;
+        try {
+            await before(args);
+            const result = await fn(...args);
+            await after(args, result);
+            return result;
+        } catch (error) {
+            // 捕获异常并传递给 after 函数
+            await after(args, undefined, error);
+            // 重新抛出异常，以便调用者可以处理
+            throw error;
+        }
     }
 }
