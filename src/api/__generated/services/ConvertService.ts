eb17fe9ce2e4a75ff1441103f0522f7019faf492
Potmot <377029227@qq.com>
2024-11-24 00:00:39 +0800

    UPDATE: 同步 api

--- src/api/__generated/services/ConvertService.ts
+++ src/api/__generated/services/ConvertService.ts
@@ -32,6 +32,13 @@ export class ConvertService {
             _uri += encodeURIComponent(_value);
             _separator = '&';
         }
+        _value = options.keepNameComment;
+        if (_value !== undefined && _value !== null) {
+            _uri += _separator
+            _uri += 'keepNameComment='
+            _uri += encodeURIComponent(_value);
+            _separator = '&';
+        }
         return (await this.executor({uri: _uri, method: 'POST', body: options.body})) as Promise<Array<number>>;
     }
 }
@@ -40,7 +47,8 @@ export type ConvertServiceOptions = {
     'convertTable': {
         body: Array<number>, 
         modelId?: number | undefined, 
-        properties?: GenConfigProperties | undefined
+        properties?: GenConfigProperties | undefined, 
+        keepNameComment?: boolean | undefined
     }, 
     'convertModel': {
         id: number, 
