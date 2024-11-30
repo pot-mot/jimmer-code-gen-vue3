58d0584ecd61cf53d4109d23f797254adcdef7b5
Potmot <377029227@qq.com>
2024-11-30 14:08:20 +0800

    UPDATE: defaultProperty UPDATE: EntityConfigForm base

--- src/components/business/entity/EntityConfigForm.vue
+++ src/components/business/entity/EntityConfigForm.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
-import {GenEntityConfigInput, GenEntityDetailView} from "@/api/__generated/model/static";
+import {
+	GenEntityConfigWithNewPropertiesInput,
+	GenEntityDetailView, type GenPropertyEntityConfigInput
+} from "@/api/__generated/model/static";
 import {FormEmits} from "@/components/global/form/FormEmits.ts";
 import {cloneDeep} from "lodash";
 import {watch, ref} from "vue";
+import EntityBusinessSelect from "@/components/business/entity/EntityBusinessSelect.vue";
+import PropertyBusinessSelect from "@/components/business/entity/PropertyBusinessSelect.vue";
+import {getDefaultProperty} from "@/components/business/entity/defaultProperty.ts";
 
 const props = defineProps<{
 	entity: GenEntityDetailView
@@ -12,17 +18,60 @@ const entity = ref<GenEntityDetailView>(
 	cloneDeep(props.entity)
 )
 
+const properties = ref<GenPropertyEntityConfigInput[]>([])
+
 watch(() => props.entity, (value) => {
 	entity.value = cloneDeep(value)
 })
 
-defineEmits<FormEmits<
-	GenEntityConfigInput,
+const emits = defineEmits<FormEmits<
+	GenEntityConfigWithNewPropertiesInput,
 	GenEntityDetailView
 >>()
+
+const handleSubmit = () => {
+	emits('submit', {
+		entity: entity.value,
+		properties: properties.value
+	})
+}
+
+const handleAddManalProperty = () => {
+	properties.value.push(getDefaultProperty())
+}
 </script>
 
 <template>
 	<el-form style="width: calc(100% - 0.5rem);">
+		<el-input v-model="entity.name"/>
+		<el-switch v-model="entity.overwriteName"/>
+		<el-input v-model="entity.comment"/>
+		<el-switch v-model="entity.overwriteComment"/>
+		<el-input v-model="entity.remark"/>
+		<EntityBusinessSelect v-model="entity"/>
+
+		<div v-for="property in entity.properties">
+			<el-input v-model="property.name"/>
+			<el-switch v-model="property.overwriteName"/>
+			<el-input v-model="property.comment"/>
+			<el-switch v-model="property.overwriteComment"/>
+			<el-input v-model="property.remark"/>
+			<el-text>{{ property.type }}</el-text>
+			<el-checkbox disabled v-model="property.type"/>
+			<PropertyBusinessSelect v-model="property"/>
+		</div>
+
+		<div v-for="property in properties">
+			<el-input v-model="property.name"/>
+			<el-switch v-model="property.overwriteName"/>
+			<el-input v-model="property.comment"/>
+			<el-switch v-model="property.overwriteComment"/>
+			<el-input v-model="property.remark"/>
+			<el-input v-model="property.type"/>
+			<el-checkbox v-model="property.typeNotNull"/>
+		</div>
+		<el-button @click="handleAddManalProperty"></el-button>
+
+		<el-button @click="handleSubmit"/>
 	</el-form>
 </template>
--- src/components/business/entity/defaultProperty.ts
+++ src/components/business/entity/defaultProperty.ts
@@ -1,4 +1,40 @@
+import type {GenPropertyEntityConfigInput} from "@/api/__generated/model/static";
 
-export const getDefaultProperty = () => {
-
+export const getDefaultProperty = (): GenPropertyEntityConfigInput => {
+    return {
+        associationType: undefined,
+        body: undefined,
+        comment: "",
+        dissociateAnnotation: undefined,
+        idGenerationAnnotation: undefined,
+        idProperty: false,
+        idView: false,
+        idViewTarget: undefined,
+        inDetailView: false,
+        inInsertInput: false,
+        inListView: false,
+        inLongAssociationInput: false,
+        inLongAssociationView: false,
+        inOptionView: false,
+        inShortAssociationView: false,
+        inSpecification: false,
+        inUpdateInput: false,
+        inputNotNull: undefined,
+        joinColumnMetas: undefined,
+        joinTableMeta: undefined,
+        keyGroup: undefined,
+        keyProperty: false,
+        listType: false,
+        logicalDelete: false,
+        longAssociation: false,
+        mappedBy: undefined,
+        name: "",
+        orderKey: 0,
+        otherAnnotation: undefined,
+        overwriteComment: false,
+        overwriteName: false,
+        remark: "",
+        type: "",
+        typeNotNull: false
+    }
 }
