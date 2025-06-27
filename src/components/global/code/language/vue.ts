3bda55406c82da364837f84b20078dc31874bff8
Potmot <377029227@qq.com>
2025-06-27 14:49:28 +0800

    UPDATE: more sincerely vueLanguage highlight UPDATE: code style

--- src/components/global/code/language/vue.ts
+++ src/components/global/code/language/vue.ts
@@ -1,5 +1,20 @@
 import * as Prism from 'prismjs';
 
 export const vueLanguage = {
-    ...Prism.languages.typescript,
+    ...Prism.languages.markup, // 基于 HTML 语言扩展
+    script: {
+        pattern: /(<script.*?>)([\s\S]*?)(?=<\/script>)/i,
+        lookbehind: true,
+        inside: Prism.languages.javascript || Prism.languages.typescript
+    },
+    style: {
+        pattern: /(<style.*?>)([\s\S]*?)(?=<\/style>)/i,
+        lookbehind: true,
+        inside: Prism.languages.css
+    },
+    template: {
+        pattern: /(<template.*?>)([\s\S]*?)(?=<\/template>)/i,
+        lookbehind: true,
+        inside: Prism.languages.markup
+    }
 }
