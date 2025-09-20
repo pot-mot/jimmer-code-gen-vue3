4789f17b00e096a66860263c9f637d3e290ec5d0
Potmot <377029227@qq.com>
2025-09-20 21:17:47 +0800

    ADD: MenuItemToTree

---
+++ src/modelEditor/menu/tree/MenuItemToTree.ts
@@ -0,0 +1,75 @@
+import type {TreeNode} from "@/components/tree/TreeNode.ts";
+import type {MenuItem} from "@/modelEditor/useModelEditor.ts";
+
+export type MenuItemTreeNode = TreeNode<{
+    type: "Group",
+    group: Group,
+} | {
+    type: "MappedSuperClass",
+    mappedSuperClass: MappedSuperClassWithProperties,
+} | {
+    type: "Entity",
+    entity: EntityWithProperties,
+} | {
+    type: "Enumeration",
+    enumeration: Enumeration,
+} | {
+    type: "EmbeddableType",
+    embeddableType: EmbeddableTypeWithProperties,
+} | {
+    type: "Association",
+    association: Association,
+}>
+
+const entityToTreeNode = (entity: EntityWithProperties): MenuItemTreeNode => {
+    return {
+        id: entity.id,
+        data: {
+            type: "Entity",
+            entity,
+        },
+    }
+}
+const mappedSuperClassToTreeNode = (mappedSuperClass: MappedSuperClassWithProperties): MenuItemTreeNode => {
+    return {
+        id: mappedSuperClass.id,
+        data: {
+            type: "MappedSuperClass",
+            mappedSuperClass,
+        },
+    }
+}
+const enumerationToTreeNode = (enumeration: Enumeration): MenuItemTreeNode => {
+    return {
+        id: enumeration.id,
+        data: {
+            type: "Enumeration",
+            enumeration,
+        },
+    }
+}
+const embeddableToTreeNode = (embeddableType: EmbeddableTypeWithProperties): MenuItemTreeNode => {
+    return {
+        id: embeddableType.id,
+        data: {
+            type: "EmbeddableType",
+            embeddableType,
+        },
+    }
+}
+
+export const menuItemToTree = (menuItem: MenuItem): MenuItemTreeNode => {
+    return {
+        id: menuItem.group.id,
+        data: {
+            type: "Group",
+            group: menuItem.group,
+        },
+        children: [
+            ...menuItem.orderedMappedSuperClasses.map(mappedSuperClassToTreeNode),
+            ...menuItem.orderedEntities.map(entityToTreeNode),
+            ...menuItem.orderedEnumerations.map(enumerationToTreeNode),
+            ...menuItem.orderedEmbeddableTypes.map(embeddableToTreeNode),
+        ]
+    }
+}
