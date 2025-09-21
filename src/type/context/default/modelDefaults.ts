5508dd0e7ccf17b9543687466350438996779f48
Potmot <377029227@qq.com>
2025-09-21 17:08:05 +0800

    UPDATE: modelDefaults

--- src/type/context/default/modelDefaults.ts
+++ src/type/context/default/modelDefaults.ts
@@ -1,5 +1,25 @@
 import {createId} from "@/modelEditor/useModelEditor.ts";
 
+export const presetColor = [
+    'rgba(100, 100, 100, 1)',
+    'rgba(150, 60, 60, 1)',
+    'rgba(200, 100, 100, 1)',
+    'rgba(150, 100, 100, 1)',
+    'rgba(115, 90, 80, 1)',
+    'rgba(200, 120, 90, 1)',
+    'rgba(180, 150, 100, 1)',
+    'rgba(100, 100, 200, 1)',
+    'rgba(130, 85, 150, 1)',
+    'rgba(100, 120, 190, 1)',
+    'rgba(100, 100, 150, 1)',
+    'rgba(100, 150, 200, 1)',
+    'rgba(90, 140, 60, 1)',
+    'rgba(100, 150, 100, 1)',
+    'rgba(100, 150, 150, 1)',
+    'rgba(70, 140, 110, 1)',
+]
+
+
 export const defaultIdProperty = (): IdProperty => ({
     id: createId("Property"),
     name: "id",
@@ -55,11 +75,61 @@ export const defaultEntity = (groupId: string): EntityWithProperties => ({
     ]
 })
 
+export const defaultMappedSuperClass = (groupId: string): MappedSuperClassWithProperties => ({
+    groupId,
+    id: createId("MappedSuperClass"),
+    name: "BaseEntity",
+    comment: "",
+    subPackagePath: "",
+    extendsIds: [],
+    extraImports: [],
+    extraAnnotations: [],
+    properties: [
+        defaultScalarProperty()
+    ]
+})
+
+export const defaultEnumerationItem = (): EnumerationItem => ({
+    id: createId("EnumerationItem"),
+    name: "Item",
+    ordinal: 1,
+    comment: "",
+    extraImports: [],
+    extraAnnotations: [],
+})
+
+export const defaultEnumeration = (groupId: string): Enumeration => ({
+    groupId,
+    id: createId("Enumeration"),
+    name: "Enumeration",
+    comment: "",
+    subPackagePath: "",
+    extraImports: [],
+    extraAnnotations: [],
+    strategy: "DEFAULT",
+    items: [
+        defaultEnumerationItem()
+    ]
+})
+
+export const defaultEmbeddableType = (groupId: string): EmbeddableTypeWithProperties => ({
+    groupId,
+    id: createId("EmbeddableType"),
+    name: "EmbeddableType",
+    comment: "",
+    subPackagePath: "",
+    extraImports: [],
+    extraAnnotations: [],
+    properties: [
+        defaultScalarProperty()
+    ]
+})
+
 export const defaultGroup = (): Group => ({
     id: createId("Group"),
-    name: "Default",
+    name: "Group",
     comment: "",
-    color: "",
+    color: presetColor[0],
     basePackagePath: "",
     baseTableSchema: ""
 })
