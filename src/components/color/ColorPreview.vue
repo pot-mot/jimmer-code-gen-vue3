32bbb02ab07d7254b08d013399bcd2c1c0c88e1b
Potmot <377029227@qq.com>
2025-10-18 23:12:13 +0800

    ADD: ColorPreview

---
+++ src/components/color/ColorPreview.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+    value: string
+}>()
+</script>
+
+<template>
+    <div class="color-preview-wrapper">
+        <div
+            class="color-preview"
+            :style="{ backgroundColor: value }"
+        />
+    </div>
+</template>
+
+<style scoped>
+.color-preview-wrapper {
+    position: relative;
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    border: var(--border);
+    vertical-align: middle;
+    user-select: none;
+}
+
+.color-preview {
+    width: 100%;
+    height: 100%;
+}
+</style>
