0625974e47fa919e526132d60b21a97232d2953a
Potmot <377029227@qq.com>
2025-10-21 21:31:23 +0800

    FIX: MappedSuperClassWithProperties jsonSchema PropertyJsonSchema

--- src/type/context/jsonSchema/MappedSuperClassWithProperties.ts
+++ src/type/context/jsonSchema/MappedSuperClassWithProperties.ts
@@ -1,7 +1,7 @@
 import type {JSONSchemaType} from "ajv/lib/types/json-schema.ts";
 import {createSchemaValidator} from "@/utils/type/typeGuard.ts";
 import MappedSuperClassJsonSchema from "@/type/__generated/jsonSchema/items/MappedSuperClass.ts"
-import PropertyJsonSchema from "@/type/__generated/jsonSchema/items/ManyToManyMappedProperty.ts"
+import PropertyJsonSchema from "@/type/__generated/jsonSchema/items/MappedSuperClassProperty.ts"
 
 const {definitions: mappedSuperClassDefinitions, properties: mappedSuperClassProperties, ...mappedSuperClassOther} = MappedSuperClassJsonSchema.schema
 const {definitions: propertyDefinitions, propertyOther} = PropertyJsonSchema.schema
