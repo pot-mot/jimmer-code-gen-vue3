7ebef1ee7806853c88d6adff992f7967f0c44b41
Potmot <377029227@qq.com>
2025-11-04 23:11:48 +0800

    UPDATE: DatabaseTypeView add Icon

--- src/components/icons/database/IconTypedDatabase.vue
+++ src/components/icons/database/IconTypedDatabase.vue
@@ -3,6 +3,7 @@
     <IconPostgreSQL v-else-if="type === 'POSTGRESQL'"/>
     <IconOracle v-else-if="type === 'ORACLE'"/>
     <IconSqlServer v-else-if="type === 'SQLSERVER'"/>
+    <IconH2 v-else-if="type === 'H2'"/>
     <IconSqlite v-else-if="type === 'SQLITE'"/>
     <IconDatabase v-else/>
 </template>
@@ -13,6 +14,7 @@ import IconMySql from "@/components/icons/database/IconMySQL.vue";
 import IconPostgreSQL from "@/components/icons/database/IconPostgreSQL.vue";
 import IconOracle from "@/components/icons/database/IconOracle.vue";
 import IconSqlServer from "@/components/icons/database/IconSqlServer.vue";
+import IconH2 from "@/components/icons/database/IconH2.vue";
 import IconSqlite from "@/components/icons/database/IconSqlite.vue";
 
 defineProps<{
--- src/modelEditor/modelForm/databaseType/DatabaseTypeView.vue
+++ src/modelEditor/modelForm/databaseType/DatabaseTypeView.vue
@@ -1,16 +1,47 @@
 <script setup lang="ts">
+import IconMySQL from "@/components/icons/database/IconMySQL.vue";
+import IconPostgreSQL from "@/components/icons/database/IconPostgreSQL.vue";
+import IconOracle from "@/components/icons/database/IconOracle.vue";
+import IconSqlServer from "@/components/icons/database/IconSqlServer.vue";
+import IconDatabase from "@/components/icons/IconDatabase.vue";
+import IconSqlite from "@/components/icons/database/IconSqlite.vue";
+import IconH2 from "@/components/icons/database/IconH2.vue";
+
 defineProps<{
    databaseType: DatabaseSource | undefined
 }>()
 </script>
 
 <template>
-    <div v-if="databaseType === 'MYSQL'">MySQL</div>
-    <div v-else-if="databaseType === 'POSTGRESQL'">PostgreSQL</div>
-    <div v-else-if="databaseType === 'ORACLE'">Oracle</div>
-    <div v-else-if="databaseType === 'SQLSERVER'">Microsoft SQL Server</div>
-    <div v-else-if="databaseType === 'H2'">H2</div>
-    <div v-else-if="databaseType === 'SQLITE'">Sqlite</div>
-    <div v-else-if="databaseType === 'ANY'">[ANY]</div>
-    <div v-else>[Unknown]</div>
+    <div v-if="databaseType === 'MYSQL'">
+        <IconMySQL/>
+        MySQL
+    </div>
+    <div v-else-if="databaseType === 'POSTGRESQL'">
+        <IconPostgreSQL/>
+        PostgreSQL
+    </div>
+    <div v-else-if="databaseType === 'ORACLE'">
+        <IconOracle/>
+        Oracle
+    </div>
+    <div v-else-if="databaseType === 'SQLSERVER'">
+        <IconSqlServer/>
+        Microsoft SQL Server
+    </div>
+    <div v-else-if="databaseType === 'H2'">
+        <IconH2/>
+        H2
+    </div>
+    <div v-else-if="databaseType === 'SQLITE'">
+        <IconSqlite/>
+        Sqlite
+    </div>
+    <div v-else-if="databaseType === 'ANY'">
+        <IconDatabase/>
+        [ANY]
+    </div>
+    <div v-else>
+        [Unknown]
+    </div>
 </template>
