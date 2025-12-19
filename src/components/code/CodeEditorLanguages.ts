eb4ba7f18ed6e951b3cc97db42e1110a27ba4a9f
Potmot <377029227@qq.com>
2025-12-19 14:49:30 +0800

    FIX: CodeEditor remove @ts-ignore

--- src/components/code/CodeEditorLanguages.ts
+++ src/components/code/CodeEditorLanguages.ts
@@ -10,18 +10,13 @@ export const CodeEditorLanguage_CONSTANTS = [
 
 export type CodeEditorLanguage = typeof CodeEditorLanguage_CONSTANTS[number]
 
-// @ts-ignore
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-// @ts-ignore
 import JSONWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-// @ts-ignore
 import CSSWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-// @ts-ignore
 import HTMLWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-// @ts-ignore
 import TSWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
-export const getWorker = (language: CodeEditorLanguage | string | undefined) => {
+export const getWorker = (language: CodeEditorLanguage | string | undefined): Worker => {
     switch (language) {
         case "typescript":
         case "javascript":
