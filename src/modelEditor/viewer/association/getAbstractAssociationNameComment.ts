3590c1b6839b892f849c1b6d3092c7ec03936245
Potmot <377029227@qq.com>
2025-12-28 21:50:43 +0800

    UPDATE: getAbstractAssociationNameComment support midTable

--- src/modelEditor/viewer/association/getAbstractAssociationNameComment.ts
+++ src/modelEditor/viewer/association/getAbstractAssociationNameComment.ts
@@ -1,7 +1,9 @@
 import {useModelEditor} from "@/modelEditor/useModelEditor.ts";
 import {
     tmpl_fkComment,
-    tmpl_fkName
+    tmpl_fkName,
+    tmpl_midTableComment,
+    tmpl_midTableName
 } from "@/type/context/utils/AssociationTemplate.ts";
 
 export const getAbstractAssociationNameComment = (association: DeepReadonly<AbstractAssociationIdOnly>) => {
@@ -11,8 +13,18 @@ export const getAbstractAssociationNameComment = (association: DeepReadonly<Abst
     const sourceProperty = sourceAbstractEntity.properties.find(it => it.id === association?.sourcePropertyId)
     if (!sourceProperty) return
 
-    let name = tmpl_fkName(association.nameTemplate, {name: `$${sourceAbstractEntity.name}$`}, sourceProperty)
-    let comment = tmpl_fkComment(association.commentTemplate, sourceAbstractEntity, sourceProperty)
+    let name: string
+    let comment: string
+    if ("joinInfo" in sourceProperty && sourceProperty.joinInfo.type === "MidTable") {
+        const referencedEntity = contextData.entityMap.get(association.referencedEntityId)
+        if (!referencedEntity) return
+
+        name = tmpl_midTableName(association.nameTemplate, {name: `$${sourceAbstractEntity.name}$`}, referencedEntity)
+        comment = tmpl_midTableComment(association.commentTemplate, sourceAbstractEntity, referencedEntity)
+    } else {
+        name = tmpl_fkName(association.nameTemplate, {name: `$${sourceAbstractEntity.name}$`}, sourceProperty)
+        comment = tmpl_fkComment(association.commentTemplate, sourceAbstractEntity, sourceProperty)
+    }
 
     return {
         id: association.id,
