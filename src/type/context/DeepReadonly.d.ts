b85ea1aa9f3e81bd7d3a842edd58777a85766fdb
Potmot <377029227@qq.com>
2026-01-06 14:46:55 +0800

    REFACTOR: Simplify DeepReadonly type

--- src/type/context/DeepReadonly.d.ts
+++ src/type/context/DeepReadonly.d.ts
@@ -5,20 +5,12 @@
 type DeepReadonly<T> =
     T extends (...args: any[]) => any
         ? T // 函数类型保持不变
-        : T extends ReadonlyMap<infer K, infer V>
-            ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>> // 处理ReadonlyMap
-            : T extends Map<infer K, infer V>
-                ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>> // 处理Map
-                : T extends ReadonlySet<infer U>
-                    ? ReadonlySet<DeepReadonly<U>> // 处理ReadonlySet
-                    : T extends Set<infer U>
-                        ? ReadonlySet<DeepReadonly<U>> // 处理Set
-                        : T extends ReadonlyArray<infer U>
-                            ? T extends Record<number, infer U>
-                                ? ReadonlyArray<DeepReadonly<U>> // 处理只读数组
-                                : never
-                            : T extends Array<infer U>
-                                ? ReadonlyArray<DeepReadonly<U>> // 处理普通数组
-                                : T extends object
-                                    ? { readonly [K in keyof T]: DeepReadonly<T[K]> } // 处理普通对象
-                                    : T; // 基本类型直接返回
+        : T extends ReadonlyMap<infer K, infer V> | Map<infer K, infer V>
+            ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>> // 处理Map
+            : T extends ReadonlySet<infer U> | Set<infer U>
+                ? ReadonlySet<DeepReadonly<U>> // 处理Set
+                : T extends ReadonlyArray<infer U> | Array<infer U>
+                    ? ReadonlyArray<DeepReadonly<U>>
+                    : T extends object
+                        ? { readonly [K in keyof T]: DeepReadonly<T[K]> } // 处理普通对象
+                        : T; // 基本类型直接返回
