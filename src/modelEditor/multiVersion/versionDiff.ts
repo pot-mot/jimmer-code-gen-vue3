69a7b9174cfbb56e7f04648c5ea1cafaa08077a1
Potmot <377029227@qq.com>
2026-01-07 18:36:06 +0800

    UPDATE：nameIdAnyMatch add nameIdBoth match

--- src/modelEditor/multiVersion/versionDiff.ts
+++ src/modelEditor/multiVersion/versionDiff.ts
@@ -19,6 +19,23 @@ const nameIdAnyMatch = [
             "data" in a && typeof a.data === "object" &&
             "data" in b && typeof b.data === "object" &&
             "name" in a.data && typeof a.data.name === "string" &&
+            "name" in b.data && typeof b.data.name === "string" &&
+            "id" in a.data && typeof a.data.id === "string" &&
+            "id" in b.data && typeof b.data.id === "string"
+        ) return a.data.name === b.data.name && a.data.id === b.data.id
+        if (
+            "name" in a && typeof a.name === "string" &&
+            "name" in b && typeof b.name === "string" &&
+            "id" in a && typeof a.id === "string" &&
+            "id" in b && typeof b.id === "string"
+        ) return a.name === b.name && a.id === b.id
+        return false
+    },
+    (a: any, b: any): boolean => {
+        if (
+            "data" in a && typeof a.data === "object" &&
+            "data" in b && typeof b.data === "object" &&
+            "name" in a.data && typeof a.data.name === "string" &&
             "name" in b.data && typeof b.data.name === "string"
         ) return a.data.name === b.data.name
         if (
